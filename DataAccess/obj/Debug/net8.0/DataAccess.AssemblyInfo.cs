57c4c1a7b2470afb27d28dc7a5ddaa6389afbf9f
hedonicadapter <filtersome@gmail.com>
2024-02-24 19:43:39 +0100

    removed stupid banner

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b102cb431c633aff16bf707181adff99a35117d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf0c6db4805de428514901033bd4545139d6cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
